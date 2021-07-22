e845fdaf08ba1edc899cb5e01975dcb02ef71f0a
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-07-22 14:46:09 -0300

    Update app SDK version and include typescript
    As long app were on first stages and without any page customization, it were restarted.

--- App.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import Routes from './src/Routes';
-
-export default function App() {
-  return (
-    <Routes />
-  );
-}
---
+++ App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Updates from "expo-updates";
+
+import Routes from './src/Routes';
+
+export default function App() {
+  useEffect(() => {
+    async function updateApp() {
+      const { isAvailable } = await Updates.checkForUpdateAsync();
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    }
+    // updateApp();
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
